1b8ea0a8104bfea1292a6580c03f7ddef79c6369
sachham <sachham3@gmail.com>
2019-08-02 17:33:32 +0700

    Data using HTTP

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
   `
 })
 export class AppComponent {
-  pageTitle = 'Chilindo Product Management';
+  pageTitle = 'Product Management';
 }
--- APM-Start/src/products/product-list.component.ts
+++ APM-Start/src/products/product-list.component.ts
@@ -15,6 +15,8 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
 
     _listFilter: string;
+    errorMessage: string;
+
     get listFilter(): string {
         return this._listFilter;
     }
@@ -44,7 +46,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe(
+            products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );
+
     }
 }
