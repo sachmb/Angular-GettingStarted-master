1de9ba39a53991b4b4177a2ff6bd76bd79181a19
sachham <sachham3@gmail.com>
2019-08-05 17:08:15 +0700

    Navigation and Routing Additional Techniques

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { R3BoundTarget } from '@angular/compiler';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,9 @@ import { R3BoundTarget } from '@angular/compiler';
     HttpClientModule,
     RouterModule.forRoot([
       {path:'products', component: ProductListComponent},
-      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'products/:id',
+      canActivate : [ProductDetailGuard],
+      component: ProductDetailComponent},
       {path: 'welcome', component: WelcomeComponent },
       {path: '', redirectTo: 'welcome', pathMatch:'full'},
       {path: '**', redirectTo: 'welcome', pathMatch:'full'}
--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,17 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from 'src/products/product';
+import { ProductService } from 'src/products/product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitel: string = 'Product Detail';
+  pageTitle: string = 'Product Detail';
+  errorMessage = '';
   product: IProduct;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) {
   }
 
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `:${id}`;
+    // if (param) {
+    //   const id = +param;
+    //   this.getProduct(id);
+    // }
+     this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+
+
+
+  }
+  onBack(): void {
+    this.router.navigate(['/products'])
+  }
 }
+
+
